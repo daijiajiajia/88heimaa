d3b84d9f5e8ae7be6b9618e1c6552126a33440d9
daijiajiajia <1969019362@qq.com>
2019-11-13 22:11:53 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,19 @@
 <template>
-<div>主页</div>
+<!-- 布局 -->
+<el-container>
+  <!-- 先左右布局 -->
+  <!-- 侧边栏布局 -->
+  <el-aside></el-aside>
+  <!-- 右边布局 -->
+  <el-container>
+    <!-- 又分为上下布局 -->
+    <el-header></el-header>
+    <el-main>
+      <!-- 二级路由容器 -->
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
