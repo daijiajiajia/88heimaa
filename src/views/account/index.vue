9ee8405cfa9c7afe2717c9d308b67fc6edacf6e7
daijiajiajia <1969019362@qq.com>
2019-11-20 18:22:20 +0800

    账户信息的处理-创建-路由-组件导入-调接口

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,76 +1,69 @@
 <template>
-<div>
-      <el-form ref="form" :model="form" label-width="80px">
-  <el-form-item label="活动名称">
-    <el-input v-model="form.name"></el-input>
-  </el-form-item>
-  <el-form-item label="活动区域">
-    <el-select v-model="form.region" placeholder="请选择活动区域">
-      <el-option label="区域一" value="shanghai"></el-option>
-      <el-option label="区域二" value="beijing"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item label="活动时间">
-    <el-col :span="11">
-      <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-    </el-col>
-  </el-form-item>
-  <el-form-item label="即时配送">
-    <el-switch v-model="form.delivery"></el-switch>
-  </el-form-item>
-  <el-form-item label="活动性质">
-    <el-checkbox-group v-model="form.type">
-      <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-      <el-checkbox label="地推活动" name="type"></el-checkbox>
-      <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-      <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-    </el-checkbox-group>
-  </el-form-item>
-  <el-form-item label="特殊资源">
-    <el-radio-group v-model="form.resource">
-      <el-radio label="线上品牌商赞助"></el-radio>
-      <el-radio label="线下场地免费"></el-radio>
-    </el-radio-group>
-  </el-form-item>
-  <el-form-item label="活动形式">
-    <el-input type="textarea" v-model="form.desc"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="onSubmit">立即创建</el-button>
-    <el-button>取消</el-button>
-  </el-form-item>
-</el-form>
-</div>
+  <div>
+    <el-form ref="form" :model="user" label-width="80px">
+      <el-form-item label="用户头像">
+        <el-upload
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+        >
+          <img width="100" :src="user.photo" class="avatar" />
+          <i class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
+      <el-form-item label="用户昵称">
+        <el-input v-model="user.name"></el-input>
+      </el-form-item>
+      <el-form-item label="个人介绍">
+        <el-input v-model="user.intro"></el-input>
+      </el-form-item>
+       <el-form-item label="手机号码">
+        <el-input v-model="user.mobile"></el-input>
+      </el-form-item>
+      <el-form-item label="用户邮箱">
+        <el-input v-model="user.email"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">保存修改</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+      user: {
+        email: '', // 邮箱
+        intro: '', // 简介
+        mobile: '', // 手机号
+        name: '', // 昵称
+        photo: '' // 头像
       }
     }
+  },
+  created () {
+    this.loadUserProfile()
   },
   methods: {
     onSubmit () {
       console.log('submit!')
+    },
+    loadUserProfile () {
+      this.$axios({
+        method: 'GET',
+        url: '/user/profile'
+      }).then(res => {
+        console.log(res.data)
+        this.user = res.data.data
+      }).catch(err => {
+        console.log(err)
+      })
     }
   }
 }
 </script>
 
 <style>
-
 </style>
