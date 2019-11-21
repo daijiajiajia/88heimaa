d1f3038f262f312666cca156eef638ad5db206c4
daijiajiajia <1969019362@qq.com>
2019-11-21 23:12:41 +0800

    发布文章-展示素材库

--- src/views/publish/components/upload-image.vue
+++ src/views/publish/components/upload-image.vue
@@ -1,23 +1,42 @@
 <template>
   <div>
-    <!-- <el-col :span='4'>
-      <el-upload
+    <el-col :span='4'>
+      <div
+        @click="onUploadshow"
         class="avatar-uploader"
         action="https://jsonplaceholder.typicode.com/posts/"
         :show-file-list="false"
+        >
         <img v-if="imageUrl" :src="imageUrl" class="avatar">
-        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-      </el-upload>
-    </el-col>-->
+        <i v-else  @click="centerDialogVisible = true" class="el-icon-plus avatar-uploader-icon"></i>
+      </div>
+    </el-col>
     <!-- <p>上传</p> -->
-    <el-button type="text" @click="centerDialogVisible = true">图片上传</el-button>
+    <!-- <el-button type="text" @click="centerDialogVisible = true">图片上传</el-button> -->
 
     <el-dialog
-    title="请选择文章封面图片"
-    :visible.sync="centerDialogVisible"
-    width="30%"
-    center>
-      <span>需要注意的是内容是默认不居中的</span>
+      title="请选择文章封面图片"
+      :visible.sync="centerDialogVisible"
+      width="50%"
+      center>
+
+      <el-tabs v-model="activeName" >
+        <el-tab-pane label="素材库" name="first">
+          <!-- 标签内容 -->
+            <el-radio-group v-model="activeType" @change='loadImages'>
+              <el-radio label="all">全部</el-radio>
+              <el-radio label="collect">收藏</el-radio>
+            </el-radio-group>
+            <el-row :gutter="20">
+              <el-col :span="6" v-for='item in images' :key='item'>
+                <img height='100' :src="item.url">
+              </el-col>
+            </el-row>
+        </el-tab-pane>
+        <el-tab-pane label="上传图片" name="second">上传图片</el-tab-pane>
+      </el-tabs>
+
+      <!-- <span>需要注意的是内容是默认不居中的</span> -->
       <span slot="footer" class="dialog-footer">
         <el-button @click="centerDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
@@ -31,32 +50,45 @@ export default {
   name: 'uploadImages',
   data () {
     return {
-      centerDialogVisible: false
+      centerDialogVisible: false,
+      activeName: 'first', // 激活标签页
+      activeType: 'all', // 激活的类型
+      images: []
     }
   },
   methods: {
-    // handleAvatarSuccess (res, file) {
-    //   this.imageUrl = URL.createObjectURL(file.raw)
-    // },
-    // beforeAvatarUpload (file) {
-    //   const isJPG = file.type === 'image/jpeg'
-    //   const isLt2M = file.size / 1024 / 1024 < 2
 
-    //   if (!isJPG) {
-    //     this.$message.error('上传头像图片只能是 JPG 格式!')
-    //   }
-    //   if (!isLt2M) {
-    //     this.$message.error('上传头像图片大小不能超过 2MB!')
-    //   }
-    //   return isJPG && isLt2M
-    // }
-  }
+    onUploadshow () {
+      // 请求加载数据
+      this.loadImages()
+
+      // 显示弹框
+      this.centerDialogVisible = true
+    },
+    loadImages (isCollect = false) {
+      this.$axios({
+        method: 'GET',
+        url: '/user/images',
+        params: {
+          //  this.activeType  z双向绑定了radio
+          collect: this.activeType === 'collect' // 是否获取收藏图片
+        }
+      }).then(res => {
+        console.log(res)
+        this.images = res.data.data.results
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  },
+  created () {},
+  mounted () {}
 }
 </script>
 
 <style scoped>
 .avatar-uploader .el-upload {
-  border: 1px solid #d9d9d9;
+  border: 1px solid red;
   border-radius: 6px;
   cursor: pointer;
   position: relative;
