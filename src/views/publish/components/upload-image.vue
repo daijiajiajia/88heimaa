c45fade796319a5ef4e0764cda772a674192c6dd
daijiajiajia <1969019362@qq.com>
2019-11-21 22:25:00 +0800

    封装上传封面组件-使用对话框

--- src/views/publish/components/upload-image.vue
+++ src/views/publish/components/upload-image.vue
@@ -1,13 +1,81 @@
 <template>
-  <div>上传图片组件</div>
+  <div>
+    <!-- <el-col :span='4'>
+      <el-upload
+        class="avatar-uploader"
+        action="https://jsonplaceholder.typicode.com/posts/"
+        :show-file-list="false"
+        <img v-if="imageUrl" :src="imageUrl" class="avatar">
+        <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+      </el-upload>
+    </el-col>-->
+    <!-- <p>上传</p> -->
+    <el-button type="text" @click="centerDialogVisible = true">图片上传</el-button>
+
+    <el-dialog
+    title="请选择文章封面图片"
+    :visible.sync="centerDialogVisible"
+    width="30%"
+    center>
+      <span>需要注意的是内容是默认不居中的</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'uploadImages'
+  name: 'uploadImages',
+  data () {
+    return {
+      centerDialogVisible: false
+    }
+  },
+  methods: {
+    // handleAvatarSuccess (res, file) {
+    //   this.imageUrl = URL.createObjectURL(file.raw)
+    // },
+    // beforeAvatarUpload (file) {
+    //   const isJPG = file.type === 'image/jpeg'
+    //   const isLt2M = file.size / 1024 / 1024 < 2
+
+    //   if (!isJPG) {
+    //     this.$message.error('上传头像图片只能是 JPG 格式!')
+    //   }
+    //   if (!isLt2M) {
+    //     this.$message.error('上传头像图片大小不能超过 2MB!')
+    //   }
+    //   return isJPG && isLt2M
+    // }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.avatar-uploader .el-upload {
+  border: 1px solid #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
